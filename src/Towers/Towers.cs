cffee6574b1e36b2b17b99e05f67b7c39eeed9d9
ShuxDev <shuxdev@gmail.com>
2021-09-17 16:36:34 +0200

    Towers

--- src/Towers/Towers.cs
+++ src/Towers/Towers.cs
@@ -7,11 +7,11 @@ namespace TowerDefenseMC.Towers
 {
     public class Towers : Node
     {
-        private readonly Texture _uiRedTower = ResourceLoader.Load<Texture>("res://assets/img/towers/towerRound_sampleC_N.png");
-        private readonly Texture _uiPurpleTower = ResourceLoader.Load<Texture>("res://assets/img/towers/towerSquare_sampleF_N.png");
+        private readonly Texture _uiRedTower = ResourceLoader.Load<Texture>("res://assets/img/towers/towerRound_sampleC.png");
+        private readonly Texture _uiPurpleTower = ResourceLoader.Load<Texture>("res://assets/img/towers/towerSquare_sampleF.png");
 
-        private readonly PackedScene _redTower = ResourceLoader.Load<PackedScene>("res://tower/RedTower.tscn");
-        private readonly PackedScene _purpleTower = ResourceLoader.Load<PackedScene>("res://tower/PurpleTower.tscn");
+        private readonly PackedScene _redTower = ResourceLoader.Load<PackedScene>("res://tower/RedTowerA.tscn");
+        private readonly PackedScene _purpleTower = ResourceLoader.Load<PackedScene>("res://tower/PurpleTowerA.tscn");
 
         public Dictionary<string, Texture> GetTower2Texture()
         {
