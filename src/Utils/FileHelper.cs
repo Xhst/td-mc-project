a5639678e31bcb4c7a3b712c2680d8330078ef1b
ShuxDev <shuxdev@gmail.com>
2021-09-23 11:42:46 +0200

    FileHelper

---
+++ src/Utils/FileHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Godot;
+
+
+namespace TowerDefenseMC.Utils
+{
+    public class FileHelper
+    {
+        public static List<string> FilesInDirectory(string dirPath)
+        {
+            List<string> fileNames = new List<string>();
+            
+            Directory dir = new Directory();
+            dir.Open(dirPath);
+            dir.ListDirBegin();
+
+            string fileName = dir.GetNext();
+            
+            while (fileName != string.Empty)
+            {
+                if (!fileName.BeginsWith("."))
+                {
+                    fileNames.Add(fileName);
+                }
+
+                fileName = dir.GetNext();
+            }
+            
+            dir.ListDirEnd();
+
+            return fileNames;
+        }
+    }
+}
