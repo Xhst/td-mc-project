c2c32b0d9db7126a955167273347a56c767cddf9
ShuxDev <shuxdev@gmail.com>
2021-10-01 10:41:37 +0200

    Refactor and edits

--- src/Main.cs
+++ src/Main.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 
 using TowerDefenseMC.Singletons;
 using TowerDefenseMC.Transitions;
@@ -6,49 +6,49 @@ using TowerDefenseMC.Transitions;
 
 namespace TowerDefenseMC
 {
-    public class Main : Node
-    {
+	public class Main : Node
+	{
 
-        private bool _initialFadeActive = true;
+		private bool _initialFadeActive = true;
 
-        public Transition Transition;
-        public Node ActiveSceneContainer;
+		public Transition Transition;
+		public Node ActiveSceneContainer;
 
-        public override void _Ready()
-        {
-            Scenes.MainScene = this;
-            
-            ActiveSceneContainer = GetNode("ActiveSceneContainer");
-            Transition = GetNode<Transition>("Transition");
+		public override void _Ready()
+		{
+			Scenes.MainScene = this;
+			
+			ActiveSceneContainer = GetNode("ActiveSceneContainer");
+			Transition = GetNode<Transition>("Transition");
 
-            Node activeScene = GetActiveScene();
-            activeScene.SetProcessInput(false);
-            activeScene.SetProcessUnhandledInput(false);
+			Node activeScene = GetActiveScene();
+			activeScene.SetProcessInput(false);
+			activeScene.SetProcessUnhandledInput(false);
 
-            if (_initialFadeActive)
-            {
-                InitialTransition();
-            }
-        }
+			if (_initialFadeActive)
+			{
+				InitialTransition();
+			}
+		}
 
-        public override void _Input(InputEvent @event)
-        {
-            if (Transition.IsPlaying())
-            {
-                GetTree().SetInputAsHandled();
-            }
-        }
+		public override void _Input(InputEvent @event)
+		{
+			if (Transition.IsPlaying())
+			{
+				GetTree().SetInputAsHandled();
+			}
+		}
 
-        private async void InitialTransition()
-        {
-            Transition.SetBlack();
-            await ToSignal(GetTree().CreateTimer(0.3f), "timeout");
-            Transition.FadeOut();
-        }
+		private async void InitialTransition()
+		{
+			Transition.SetBlack();
+			await ToSignal(GetTree().CreateTimer(0.3f), "timeout");
+			Transition.FadeOut();
+		}
 
-        public Node GetActiveScene()
-        {
-            return ActiveSceneContainer.GetChild(0);
-        }
-    }
+		public Node GetActiveScene()
+		{
+			return ActiveSceneContainer.GetChild(0);
+		}
+	}
 }
