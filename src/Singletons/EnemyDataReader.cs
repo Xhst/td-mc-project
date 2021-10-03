621699c318023fe043ef000cdb5d2d8612dc6995
ShuxDev <shuxdev@gmail.com>
2021-10-03 11:40:02 +0200

    Enemies images and EnemyDataReader

---
+++ src/Singletons/EnemyDataReader.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+using Godot;
+
+using Newtonsoft.Json.Linq;
+
+namespace TowerDefenseMC.Singletons
+{
+    public struct EnemyData
+    {
+        public string Name;
+        public string Image;
+        public float Health;
+        public float Speed;
+        public float Damage;
+        public int Feed;
+    }
+    
+    public class EnemyDataReader : Node
+    {
+
+        public Dictionary<string, EnemyData> GetEnemiesData()
+        {
+            Dictionary<string, EnemyData> enemies = new Dictionary<string, EnemyData>();
+            
+            string path = ProjectSettings.GlobalizePath("res://assets/data/enemies/enemies.json");
+            string jsonFileText = System.IO.File.ReadAllText(path);
+            
+            JObject json = JObject.Parse(jsonFileText);
+
+            if (json["enemies"] is null) return enemies;
+
+            foreach (JToken enemy in json["enemies"])
+            {
+                EnemyData enemyData = GetEnemyData(enemy);
+
+                if (enemyData.Name == string.Empty) continue;
+                
+                enemies.Add(enemyData.Name, enemyData);
+            }
+
+            return enemies;
+        }
+
+        private EnemyData GetEnemyData(JToken enemy)
+        {
+            EnemyData enemyData;
+
+            enemyData.Name = enemy["name"]?.ToString() ?? "";
+            enemyData.Image = enemy["image"]?.ToString() ?? "enemy_ufoGreen";
+            enemyData.Damage = float.Parse(enemy["damage"]?.ToString() ?? "1");
+            enemyData.Health = float.Parse(enemy["health"]?.ToString() ?? "1");
+            enemyData.Speed = float.Parse(enemy["speed"]?.ToString() ?? "100");
+            enemyData.Feed = int.Parse(enemy["feed"]?.ToString() ?? "0");
+
+            return enemyData;
+        }
+    }
+}
