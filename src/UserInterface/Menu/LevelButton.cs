393a9a1195d0a2bf20d3c756b77ae6a0d838c601
ShuxDev <shuxdev@gmail.com>
2021-10-09 16:15:38 +0200

    Level stars

--- src/UserInterface/Menu/LevelButton.cs
+++ src/UserInterface/Menu/LevelButton.cs
@@ -8,25 +8,23 @@ namespace TowerDefenseMC.UserInterface.Menu
         private LevelsMenu _menu;
         
         private int _targetLevel;
-        private int _stars;
 
         private Button _button;
+        private TextureProgress _stars;
 
         public void Init(LevelsMenu menu, int level, int stars, bool disabled = false)
         {
             _menu = menu;
             _targetLevel = level;
-            _stars = stars;
 
             _button = GetNode<Button>("LevelButton");
             _button.Text = level.ToString();
             _button.Disabled = disabled;
+            
+            _stars = GetNode<TextureProgress>("LevelButton/Stars");
+            _stars.Value = (float) stars / 6 * 100;
         }
-
-        public Button GetButton()
-        {
-            return _button;
-        }
+        
         public void OnLevelButtonPressed()
         {
             _menu.OnLevelButtonPressed(_targetLevel);
